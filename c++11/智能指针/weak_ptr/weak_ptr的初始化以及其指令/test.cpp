ae310de15e14c8a12bb4e7f6d004390eb2917aa1
kiuwu95 <moziwa3301649843@gmail.com>
2026-08-22 01:43:32 +0800

    Refactor and remove multiple C++11 feature demonstration files
    - Deleted various test files demonstrating C++11 features including:
      - `constexpr`
      - `final` and `override`
      - `initializer_list`
      - `lambda expressions`
      - `move` and `forward`
      - `nullptr`
      - `using`
      - `delegating constructors`
      - `direct initialization`
      - `inherited constructors`
      - `callable objects` and their wrappers
      - `bind` usage
      - `rvalue references`
      - `range-based for loops`
      - `raw string literals`
      - `smart pointers` including `shared_ptr`, `unique_ptr`, and `weak_ptr`
    
    - Added new test files for C++11 features to improve clarity and organization.

---
+++ c++11/智能指针/weak_ptr/weak_ptr的初始化以及其指令/test.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+#include<memory>
+using namespace std;
+
+int main() {
+    //weak_ptr的初始化
+    shared_ptr<int>ptr1 = make_shared<int>(10);
+    weak_ptr<int>ptr2(ptr1);
+    weak_ptr<int>ptr3 = ptr2;//ptr3->ptr2->ptr1->[内存]
+
+    //lock：weak_ptr所监管的指针所指向的对象
+    shared_ptr<int>ptr4 = ptr2.lock();
+    //use_count：weak_ptr所监管的指针数量
+    cout << ptr2.use_count() << endl;
+    //清空对象，使其不检测任何资源
+    ptr2.reset();
+    //expired：ture代表资源已被清空，flase则代表没有被清空
+    cout << ptr2.expired() << endl;
+}
---
+++ c++11/智能指针/weak_ptr/weak_ptr管理的重复析构问题/test.cpp
@@ -0,0 +1,52 @@
+﻿#include<iostream>
+#include<memory>
+using namespace std;
+
+class test :public enable_shared_from_this<test> {
+public:
+    shared_ptr<test> get_ptr() {
+        //return shared_ptr<test>(this)错误,也会导致重复析构,必须继承enable_shared_from_this<test>然后使用shared_from_this()。
+        //原理：在构造对于test类的共享指针的同时，由于继承了enable_shared_from_this<test>，test在构造中会产生一个weak_ptr去监测新产生的shared_ptr,
+        //weak_ptr会通过weak_ptr.lock()返回检测的对象从而避免重复析构
+        return shared_ptr<test>(shared_from_this());
+    }
+};
+
+class B;
+class A {
+public:
+    shared_ptr<B>next;
+    ~A() {
+        cout << "A析构函数的调用" << endl;
+    }
+};
+class B {
+public:
+    //shared_ptr<A>next错误，会导致主程序结束后A和B的指针数量始终为1从而导致内存泄漏，必须使用weak_ptr，因为weak_ptr不会使对象的指针数量+1。
+    weak_ptr<A>next;
+    ~B() {
+        cout << "B析构函数的调用" << endl;
+    }
+};
+
+int main() {
+    //关于在同一原始地址初始化多个智能指针对象的问题
+    int* p = new int;
+    shared_ptr<int>ptr1(p);
+    //shared_ptr<int>ptr2(p)错误，会导致重复析构，两个指针指针的use_count()的值都为1
+    shared_ptr<int>ptr2(ptr1);
+
+    //关于返回管理this的智能指针对象问题
+    shared_ptr<test>ptr3(new test);
+    shared_ptr<test>ptr4 = ptr3->get_ptr();
+
+    //循环引用问题
+    shared_ptr<A>Aptr(new A);
+    shared_ptr<B>Bptr(new B);
+    cout << "Aptr：" << Aptr.use_count() << endl;
+    cout << "Bptr：" << Bptr.use_count() << endl;
+    Aptr->next = Bptr;
+    Bptr->next = Aptr;
+    cout << "Aptr：" << Aptr.use_count() << endl;
+    cout << "Bptr：" << Bptr.use_count() << endl;
+}
