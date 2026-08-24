33adb1e4a737cac084cf6e1035edec115b385d63
kiuwu95 <moziwa3301649843@gmail.com>
2026-08-24 18:39:41 +0800

    添加友元类和强枚举类型示例程序以演示C++11特性

---
+++ c++11/友元的改进/test.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+using namespace std;
+class A;
+class B {
+    //在类中声明另一个类为友元时不再需要class关键字，还可使用别名
+    friend B;
+};
+//在类中还可通过模板声明友元,这样在那个友元类里面就可以调用这个类里面的私有属性的数据
+template<class T>
+class test {
+    friend T;
+private:
+    int m_data;
+};
---
+++ c++11/强枚举类型/test.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+using namespace std;
+
+//强枚举类型默认的底层类型为 int,但也可以显示地指定其他类型，比如：
+enum class Colors :char { Red, Blue, Green };
+
+enum class Fruits :int { Apple, Banana, Lemon };
+//强枚举类型可以避免因两个不同的枚举出现同一个名字相同的对象而导致的枚举混淆。
+//强枚举类型可以通过声明枚举域来判断该同名对象是属于哪个枚举
+enum class China { Shanghai, Dongjing, Beijing, Nanjing, };
+enum class Japan :char { Dongjing, Daban, Hengbin, Fudao };
+
+int main() {
+    //使用时必须作强制类型转换，强枚举类型不会自动作隐式类型转换
+    int num = (int)Fruits::Banana;//1
+    if (num == 1)cout << "It's a banana!" << endl;
+
+    char c = (char)Colors::Blue;
+    c += 50;
+    cout << c << endl;//'3'
+
+    Colors c1 = Colors::Blue;
+    Fruits f1 = Fruits::Apple;
+}
