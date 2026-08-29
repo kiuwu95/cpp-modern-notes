40fc14ceb398d610f1053c98c1f914fd07f15b6c
kiuwu95 <moziwa3301649843@gmail.com>
2026-08-29 22:18:08 +0800

    添加互斥量示例程序，演示C++多线程编程中数据共享问题的解决方案

---
+++ c++多线程编程/互斥量解决多线程数据共享问题/test.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+#include<thread>
+#include<mutex>
+
+int a = 0;
+std::mutex m;
+void plus() {
+    for (int i = 0;i < 1000000;i++) {
+        //加入互斥锁，解决多线程数据共享问题
+        m.lock();//加锁
+        a++;
+        m.unlock();//解锁
+    }
+}
+
+int main() {
+    std::thread t1(plus);//t1开始运行
+    std::thread t2(plus);//t2开始运行
+    t1.join();//主线程等待t1运行结束，此时t1与t2同时运行
+    t2.join();//主线程等待t2运行结束，此时t1已经结束，而t2仍在运行
+    std::cout << a << std::endl;
+}
---
+++ c++多线程编程/线程函数中的数据未定义错误/test.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+#include<thread>
+#include<functional>
+#include<memory>
+
+void func(int& a) {
+    a++;
+    return;
+}
+void func1(int* a) {
+    std::cout << *a << std::endl;
+}
+class test {
+public:
+    void print() {
+        std::cout << "Hello,world!" << std::endl;
+        return;
+    }
+};
+
+int main() {
+    int a = 1;
+    std::thread t1(func, std::ref(a));//不加ref会被模板参数推导会变成值拷贝，而不是引用
+    t1.join();
+    std::cout << a << std::endl;
+    //类与线程
+    test test1;
+    std::thread t2(&test::print, &test1);
+    t2.join();
+    //+智能指针
+    std::shared_ptr<test>ptr(new test);
+    std::thread t3(&test::print, ptr);
+    t3.join();
+    ptr.reset();
+    //普通指针
+    int* ptr1 = new int(1);
+    std::thread t4(func1, ptr1);
+    //不能在这行写delete ptr1;，否则会出现未定义行为
+    t4.join();
+    delete ptr1;
+}
