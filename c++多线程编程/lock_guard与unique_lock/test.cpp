27c451dcb9712e312b1021616016111aec7b70bd
kiuwu95 <moziwa3301649843@gmail.com>
2026-08-30 01:11:16 +0800

    添加timed_mutex和mutex示例程序，演示C++多线程编程中的锁机制

---
+++ c++多线程编程/lock_guard与unique_lock/test.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+#include<thread>
+#include<chrono>
+#include<mutex>
+
+int data1 = 0;
+std::timed_mutex mut1;//timed_mutex时间互斥锁
+void func1() {
+    for (int i = 0;i < 2;i++) {
+        //std::unique_lock <std::timed_mutex> ul(mut1)是默认构造时加锁的状态
+        //std::unique_lock <std::timed_mutex> ul(mut1,std::defer_lock)是默认构造时不加锁的状态,defer为"延迟"的意思
+        //std::unique_lock <std::timed_mutex> ul(mut1, std::adopt_lock)是强调加锁的状态
+        //std::unique_lock <std::timed_mutex> ul(mut1,std::try_to_lock)是指尝试加锁。如果可以加锁那就成功，否则就不成功
+        std::unique_lock <std::timed_mutex> ul(mut1, std::defer_lock);
+        //ul.try_lock_for(std::chrono::seconds(2))为尝试等待2s去拿到data1的使用权，如果拿到了就锁，拿不到就不锁
+        if (ul.try_lock_for(std::chrono::seconds(2))) {
+            std::this_thread::sleep_for(std::chrono::seconds(3));
+            data1++;
+        }
+    }
+    return;
+}
+
+int data2 = 0;
+std::mutex mut2;//timed_mutex时间互斥锁
+void func2() {
+    for (int i = 0;i < 1000000;i++) {
+        // std::lock_guard<std::timed_mutex>lg(mut,std::adopt_lock)是默认构造时不加锁的状态
+        std::lock_guard<std::mutex>lg(mut2);//
+        data2++;
+    }
+    return;
+}
+
+int main() {
+    std::thread t1(func1);
+    std::thread t2(func1);
+    t1.join();
+    t2.join();
+
+    std::cout << data1 << std::endl;
+
+    std::thread t3(func2);
+    std::thread t4(func2);
+    t3.join();
+    t4.join();
+
+    std::cout << data2 << std::endl;
+}
