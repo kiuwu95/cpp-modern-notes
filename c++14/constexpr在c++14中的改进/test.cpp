dd92e655815ba7cb99363ee84aa8459a92ef570f
kiuwu95 <3301649843@qq.com>
2026-09-09 17:02:37 +0800

    添加C++14编译期常量应用场景示例程序，演示位域和对齐的用法

---
+++ c++14/constexpr在c++14中的改进/test.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+using namespace std;
+//1.c++14可以在constexpr修饰的函数中使用if-else、switch、for等，而c++11只能存在return那一行代码
+constexpr int func(int num, int times) {
+    //计算num的times次方
+    int res = 1;
+    for (int i = 1;i <= times;i++, res *= num);
+    return res;
+}
+//2.c++14中，constexpr可以修饰void类型的函数，而在c++11中不可以
+constexpr void func2() {
+    cout << "Hello,world!" << endl;
+}
+//3.c++11中，对类内函数施加constexpr进行修饰会默认在该函数后面添加const修饰，即该函数内无法修改该类中的成员变量。
+//而在c++14中，则不会使用const默认修饰
+class test {
+public:
+    constexpr void print() {
+        cout << "Test,test." << endl;
+    }
+    //4.c++11中，被constexpr修饰的构造函数中只能存在初始化列表赋值内容，函数体内为空
+    //c++14中则可以在函数体中写些内容
+    constexpr test(int data) :m_data(data) {
+        cout << "test has been created!" << endl;
+    }
+private:
+    int m_data;
+};
---
+++ c++14/编译期常量应用场景举例/test.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+using namespace std;
+struct test1 {
+    // 位域宽度（指定一个整型成员使用多少个比特（bit）来存储数据）：unsigned int x : WIDTH
+    unsigned int x : 4;//4比特，最大能储存15（1111）
+};
+struct alignas(8) test2 {//以8字节对齐
+    int x;//4字节
+    char str[13];//13字节
+    double y;//8字节
+    char z;//1字节
+};
+int main() {
+    // 一些需要编译时常量的场景：
+    // 数组大小：int array[N];
+    // 模板非类型参数：template<int N>
+    // case标签：case VALUE :
+    // 位域宽度（指定一个整型成员使用多少个比特（bit）来存储数据）：unsigned int x : WIDTH
+    test1 t1;
+    t1.x = 15;
+    // 枚举值初始化：enum E{ val = N };
+    // 对齐说明符：alignas(N) 是C++11引入的对齐说明符，用于显式指定变量、类、结构体或数组在内存中的对齐方式
+    test2 t2;
+    //8的倍数对齐
+    cout << sizeof(t2) << endl;//最终大小为40字节，x和str共占了17字节，比16多出来的那一个字节因为8字节对齐原则所以要空出7字节的空间，之后才到y
+}
