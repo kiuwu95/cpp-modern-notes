9b37594f01266de72d40340fe99d60f8217e4854
kiuwu95 <3301649843@qq.com>
2026-09-11 20:58:14 +0800

    添加变量模板示例程序，演示C++14中模板全特化和聚合类的用法

---
+++ c++14/变量模板/模板全特化/test.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+//定义一个计算类型大小的模板为例：
+//主模板
+template<class T>
+constexpr size_t Typesize = sizeof(T);
+//特化模板：比较特别的可以单独写出来作特化
+template<>
+constexpr size_t Typesize < vector<int>> = 114514;//假设对于vector<int>类型其大小为114514
+
+//返回依赖模板的参数类型
+//以计算圆的周长为例：
+template<class T, T r>
+constexpr T calculateCircumference = 2 * 3.1415926535 * r;
+
+int main() {
+    cout << Typesize<int> << endl << Typesize<float> << endl << Typesize<vector<int>> << endl;
+
+    cout << calculateCircumference<int, 4> << endl//非类型参数不能为float和double，直到c++20为止
+        << calculateCircumference<int, 5> << endl
+        << calculateCircumference<int, 1> << endl;
+}
---
+++ c++14/变量模板/简单的变量模板的使用/test.cpp
@@ -0,0 +1,11 @@
+﻿#include<iostream>
+using namespace std;
+
+template<class T>
+T get_value = static_cast<T>(3.1415926535L);
+int v1 = get_value<int>;
+float v2 = get_value<float>;
+double v3 = get_value<double>;
+int main() {
+    cout << v1 << endl << v2 << endl << v3 << endl;
+}
